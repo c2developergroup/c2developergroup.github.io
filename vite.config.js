bc4769b894d182ed6953de7404be1b6dca1dd177
bynx <sebastiancastrorajbe@gmail.com>
2023-11-25 14:17:25 -0400

    fix deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/c2developergroup.github.io",
+  base: "/",
   plugins: [react()],
 });
