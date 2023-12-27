43a7504638485f19a4cb46f3815bfe7d78c5f501
hugo-c2dev <hugo.c2dev@gmail.com>
2023-12-27 01:00:53 +0000

    advancing astro

--- src/components/MarqueeCustomers.tsx
+++ src/components/MarqueeCustomers.tsx
@@ -1,19 +1,25 @@
 import "react";
-import '../css/marquee.css'
+import "../css/marquee.css";
 
 export interface MarqueeLogo {
-  src: string,
-  class: string,
+  src: string;
+  class: string;
 }
 
-export const MarqueeComponent = ({ logosTop, logosBtm }: { logosTop: MarqueeLogo[], logosBtm: MarqueeLogo[] }) => {
+export const MarqueeComponent = ({
+  logosTop,
+  logosBtm,
+}: {
+  logosTop: MarqueeLogo[];
+  logosBtm: MarqueeLogo[];
+}) => {
   const cardsTop = logosTop.map((item: MarqueeLogo, index: number) => (
     <img key={index} src={item.src} className={item.class} />
   ));
   const cardsBtm = logosBtm.map((item: MarqueeLogo, index: number) => (
     <img key={index} src={item.src} className={item.class} />
   ));
-  
+
   return (
     <div className="marquee__parent">
       <br></br>
@@ -24,14 +30,14 @@ export const MarqueeComponent = ({ logosTop, logosBtm }: { logosTop: MarqueeLogo
           <div className="marquee__group">{cardsTop}</div>
           <div className="marquee__group">{cardsTop}</div>
         </div>
-        <div className="marquee marquee--reverse">
+        {/* <div className="marquee marquee--reverse">
           <div className="marquee__group">{cardsBtm}</div>
           <div className="marquee__group">{cardsBtm}</div>
-        </div>
+        </div> */}
       </article>
       <br></br>
       <br></br>
       <br></br>
     </div>
   );
-}
+};
