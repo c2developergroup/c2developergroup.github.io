7d3b217f848e0f91a4ad33f0f1af02a2067bad7b
hugo-c2dev <hugo.c2dev@gmail.com>
2023-12-27 23:16:47 +0000

    finish redesign with astro

--- src/components/MarqueeCustomers.tsx
+++ src/components/MarqueeCustomers.tsx
@@ -4,21 +4,25 @@ import "../css/marquee.css";
 export interface MarqueeLogo {
   src: string;
   class: string;
+  href?: string;
 }
 
-export const MarqueeComponent = ({
-  logosTop,
-  logosBtm,
-}: {
-  logosTop: MarqueeLogo[];
-  logosBtm: MarqueeLogo[];
-}) => {
-  const cardsTop = logosTop.map((item: MarqueeLogo, index: number) => (
-    <img key={index} src={item.src} className={item.class} />
-  ));
-  const cardsBtm = logosBtm.map((item: MarqueeLogo, index: number) => (
-    <img key={index} src={item.src} className={item.class} />
-  ));
+export const MarqueeComponent = ({ logosTop }: { logosTop: MarqueeLogo[] }) => {
+  const cardsTop = logosTop.map((item: MarqueeLogo, index: number) => {
+    const logo = <img key={index} src={item.src} className={item.class} />;
+    if (item.href !== undefined) {
+      return (
+        <a href={item.href} target="_blank">
+          {logo}
+        </a>
+      );
+    }
+    return logo;
+  });
+
+  // const cardsBtm = logosBtm.map((item: MarqueeLogo, index: number) => (
+  //   <img key={index} src={item.src} className={item.class} />
+  // ));
 
   return (
     <div className="marquee__parent">
